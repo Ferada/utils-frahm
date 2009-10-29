319a80adf45ef8f4c9eff16528647b06deccca69
Olof-Joachim Frahm <Olof.Frahm@web.de>
2009-10-29 12:18:24 +0100

    Changed DEFCONSTANT to better variant on loading.

--- html.lisp
+++ html.lisp
@@ -4,13 +4,12 @@
   (defun make-doctype (root public system)
     (conc "<!DOCTYPE " root " PUBLIC \"" public "\" \"" system "\">")))
 
-(eval-when (:compile-toplevel)
-  (defconstant +dtd-xhtml-11-public+ "-//W3C//DTD XHTML 1.1//EN")
-  (defconstant +dtd-xhtml-11-system+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd")
-  (defconstant +dtd-xhtml-11+ (make-doctype "html" +dtd-xhtml-11-public+ +dtd-xhtml-11-system+))
+(defconstant* +dtd-xhtml-11-public+ "-//W3C//DTD XHTML 1.1//EN")
+(defconstant* +dtd-xhtml-11-system+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd")
+(defconstant* +dtd-xhtml-11+ (make-doctype "html" +dtd-xhtml-11-public+ +dtd-xhtml-11-system+))
 
-  (defconstant +decl-xml-10+ "<?xml version=\"1.0\" encoding=\"utf-8\"?>")
+(defconstant* +decl-xml-10+ "<?xml version=\"1.0\" encoding=\"utf-8\"?>")
 
-  (defconstant +xmlns-xhtml+ "http://www.w3.org/1999/xhtml")
+(defconstant* +xmlns-xhtml+ "http://www.w3.org/1999/xhtml")
 
-  (defconstant +prologue-xhtml-11+ (conc +decl-xml-10+ +dtd-xhtml-11+)))
+(defconstant* +prologue-xhtml-11+ (conc +decl-xml-10+ +dtd-xhtml-11+))
